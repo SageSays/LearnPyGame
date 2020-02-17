f997d3cfcddb4b88095881d576276edf46e624b7
Ivan Parshin-Borodin <ivan_parshin-borodin@epam.com>
2020-02-17 15:28:59 +0300

    Доработал перемещение и отрисовку "вражеских" прямоугольников

--- Breakout/app.py
+++ Breakout/app.py
@@ -1,3 +1,5 @@
+from random import Random
+
 import pygame
 
 pygame.init()
@@ -10,6 +12,11 @@ usr_w = 60
 usr_h = 10
 usr_x = width // 3
 usr_y = height - 100 - usr_h
+yy = 10
+
+
+bloch_with = 60
+bloch_hight = 10
 
 clock = pygame.time.Clock()
 
@@ -17,6 +24,22 @@ make_jump = False
 pause = False
 jump_counter = 30
 
+bg_image = pygame.image.load(r'source\image\bg_image.png')
+
+
+
+class Rectangle:
+    width = 60
+    hight = 15
+    pos_x = 0
+    pos_y = 0
+
+    def __init__(self, x, y, color_r, color_g, color_b):
+        self.pos_x = x
+        self.pos_y = y
+        self.color_r = color_r
+        self.color_g = color_g
+        self.color_b = color_b
 
 def jump():
     global usr_y, make_jump, jump_counter
@@ -35,7 +58,7 @@ def moove(arg):
     usr_x += arg
 
 def check_event():
-    global make_jump, pause
+    global make_jump, pause, usr_x
 
     keys = pygame.key.get_pressed()
 
@@ -43,10 +66,20 @@ def check_event():
         make_jump = True
 
     if keys[pygame.K_LEFT]:
-        moove(-20)
+        if (usr_x >= 3):
+            print('usr_x = {}'.format(usr_x))
+            moove(-20)
+        else:
+            print('in else left')
+            usr_x = 795
 
     if keys[pygame.K_RIGHT]:
-        moove(20)
+        if (usr_x <= 795):
+            print('usr_x = {}'.format(usr_x))
+            moove(20)
+        else:
+            print('in else right')
+            usr_x = 3
 
     if keys[pygame.K_UP]:
         make_jump = True
@@ -60,42 +93,42 @@ def check_event():
 
 def run_game():
     global usr_x
-    i = 1
-
-    color_1 = 1
-    color_2 = 88
-    color_3 = 40
+    const_x = 800
+    const_y = 400
+    coord_xx = 13
+    list_rect = []
+    coord_yy = 10
+    random = Random()
+    while ((coord_yy + 50) <= const_y):
+        while ((coord_xx + 60) <= const_x):
+            list_rect.append(
+                Rectangle(coord_xx,
+                          coord_yy,
+                          random.randint(0,255),
+                          random.randint(0,255),
+                          random.randint(0,255)
+                          )
+            )
+            coord_xx += 65
+        coord_xx = 13
+        coord_yy += 25
 
     while 1:
-
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.QUIT
                 quit()
 
-        if usr_x <= 0 + usr_w:
-            usr_x += 5
-        elif usr_x >= width - usr_w:
-            print("In elif!")
-            usr_x -= 5
-
         check_event()
 
-        if make_jump:
-            jump()
-        if color_1 >= 200:
-            i *= -1
-        elif color_1 <= 0:
-            i *= -1
-        display.fill((color_1, color_2, color_3))
-
+        display.blit(bg_image, (0,0))
         pygame.draw.rect(display, (247, 240, 22), (usr_x, usr_y, usr_w, usr_h))
+        for rect in list_rect:
+            # print('x = {0}, y = {1}'.format(rect.pos_x, rect.pos_y))
+            pygame.draw.rect(display, (rect.color_r, rect.color_g, rect.color_b), (rect.pos_x, rect.pos_y, rect.width, rect.hight))
 
         pygame.display.update()
         clock.tick(60)
-        color_1+=i
-
 
 
 
